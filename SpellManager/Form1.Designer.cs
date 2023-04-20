a3011e083ed62060854ac7764824a4f066ddc367
Yotty0404 <tyuke9216@hogmail.co.jp>
2023-04-20 18:44:26 +0900

    プロジェクト ファイルを追加します。

---
+++ SpellManager/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace SpellManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.capS1 = new System.Windows.Forms.Label();
+            this.btnS1 = new System.Windows.Forms.Button();
+            this.lblS1 = new System.Windows.Forms.Label();
+            this.lblS2 = new System.Windows.Forms.Label();
+            this.btnS2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblS3 = new System.Windows.Forms.Label();
+            this.btnS3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblS4 = new System.Windows.Forms.Label();
+            this.btnS4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblS5 = new System.Windows.Forms.Label();
+            this.btnS5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // capS1
+            // 
+            this.capS1.AutoSize = true;
+            this.capS1.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold);
+            this.capS1.ForeColor = System.Drawing.Color.White;
+            this.capS1.Location = new System.Drawing.Point(6, 14);
+            this.capS1.Name = "capS1";
+            this.capS1.Size = new System.Drawing.Size(41, 23);
+            this.capS1.TabIndex = 2;
+            this.capS1.Text = "TOP";
+            this.capS1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnS1
+            // 
+            this.btnS1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnS1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnS1.BackgroundImage")));
+            this.btnS1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnS1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnS1.Location = new System.Drawing.Point(51, 6);
+            this.btnS1.Name = "btnS1";
+            this.btnS1.Size = new System.Drawing.Size(40, 40);
+            this.btnS1.TabIndex = 0;
+            this.btnS1.UseVisualStyleBackColor = false;
+            this.btnS1.Click += new System.EventHandler(this.btnS1_Click);
+            // 
+            // lblS1
+            // 
+            this.lblS1.BackColor = System.Drawing.Color.Transparent;
+            this.lblS1.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblS1.ForeColor = System.Drawing.Color.White;
+            this.lblS1.Location = new System.Drawing.Point(49, 2);
+            this.lblS1.Name = "lblS1";
+            this.lblS1.Size = new System.Drawing.Size(47, 47);
+            this.lblS1.TabIndex = 1;
+            this.lblS1.Text = "5:00";
+            this.lblS1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblS1.Visible = false;
+            // 
+            // lblS2
+            // 
+            this.lblS2.BackColor = System.Drawing.Color.Transparent;
+            this.lblS2.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblS2.ForeColor = System.Drawing.Color.White;
+            this.lblS2.Location = new System.Drawing.Point(49, 50);
+            this.lblS2.Name = "lblS2";
+            this.lblS2.Size = new System.Drawing.Size(47, 47);
+            this.lblS2.TabIndex = 4;
+            this.lblS2.Text = "5:00";
+            this.lblS2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblS2.Visible = false;
+            // 
+            // btnS2
+            // 
+            this.btnS2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnS2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnS2.BackgroundImage")));
+            this.btnS2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnS2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnS2.Location = new System.Drawing.Point(51, 54);
+            this.btnS2.Name = "btnS2";
+            this.btnS2.Size = new System.Drawing.Size(40, 40);
+            this.btnS2.TabIndex = 3;
+            this.btnS2.UseVisualStyleBackColor = false;
+            this.btnS2.Click += new System.EventHandler(this.btnS2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "JG";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblS3
+            // 
+            this.lblS3.BackColor = System.Drawing.Color.Transparent;
+            this.lblS3.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblS3.ForeColor = System.Drawing.Color.White;
+            this.lblS3.Location = new System.Drawing.Point(49, 98);
+            this.lblS3.Name = "lblS3";
+            this.lblS3.Size = new System.Drawing.Size(47, 47);
+            this.lblS3.TabIndex = 7;
+            this.lblS3.Text = "5:00";
+            this.lblS3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblS3.Visible = false;
+            // 
+            // btnS3
+            // 
+            this.btnS3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnS3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnS3.BackgroundImage")));
+            this.btnS3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnS3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnS3.Location = new System.Drawing.Point(51, 102);
+            this.btnS3.Name = "btnS3";
+            this.btnS3.Size = new System.Drawing.Size(40, 40);
+            this.btnS3.TabIndex = 6;
+            this.btnS3.UseVisualStyleBackColor = false;
+            this.btnS3.Click += new System.EventHandler(this.btnS3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(6, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "MID";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblS4
+            // 
+            this.lblS4.BackColor = System.Drawing.Color.Transparent;
+            this.lblS4.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblS4.ForeColor = System.Drawing.Color.White;
+            this.lblS4.Location = new System.Drawing.Point(49, 146);
+            this.lblS4.Name = "lblS4";
+            this.lblS4.Size = new System.Drawing.Size(47, 47);
+            this.lblS4.TabIndex = 10;
+            this.lblS4.Text = "5:00";
+            this.lblS4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblS4.Visible = false;
+            // 
+            // btnS4
+            // 
+            this.btnS4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnS4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnS4.BackgroundImage")));
+            this.btnS4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnS4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnS4.Location = new System.Drawing.Point(51, 150);
+            this.btnS4.Name = "btnS4";
+            this.btnS4.Size = new System.Drawing.Size(40, 40);
+            this.btnS4.TabIndex = 9;
+            this.btnS4.UseVisualStyleBackColor = false;
+            this.btnS4.Click += new System.EventHandler(this.btnS4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(6, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "ADC";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblS5
+            // 
+            this.lblS5.BackColor = System.Drawing.Color.Transparent;
+            this.lblS5.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblS5.ForeColor = System.Drawing.Color.White;
+            this.lblS5.Location = new System.Drawing.Point(49, 194);
+            this.lblS5.Name = "lblS5";
+            this.lblS5.Size = new System.Drawing.Size(47, 47);
+            this.lblS5.TabIndex = 13;
+            this.lblS5.Text = "5:00";
+            this.lblS5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblS5.Visible = false;
+            // 
+            // btnS5
+            // 
+            this.btnS5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnS5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnS5.BackgroundImage")));
+            this.btnS5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnS5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnS5.Location = new System.Drawing.Point(51, 198);
+            this.btnS5.Name = "btnS5";
+            this.btnS5.Size = new System.Drawing.Size(40, 40);
+            this.btnS5.TabIndex = 12;
+            this.btnS5.UseVisualStyleBackColor = false;
+            this.btnS5.Click += new System.EventHandler(this.btnS5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(6, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 23);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "SUP";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(120, 256);
+            this.Controls.Add(this.lblS5);
+            this.Controls.Add(this.btnS5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblS4);
+            this.Controls.Add(this.btnS4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblS3);
+            this.Controls.Add(this.btnS3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblS2);
+            this.Controls.Add(this.btnS2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblS1);
+            this.Controls.Add(this.btnS1);
+            this.Controls.Add(this.capS1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label capS1;
+        private System.Windows.Forms.Button btnS1;
+        private System.Windows.Forms.Label lblS1;
+        private System.Windows.Forms.Label lblS2;
+        private System.Windows.Forms.Button btnS2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblS3;
+        private System.Windows.Forms.Button btnS3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblS4;
+        private System.Windows.Forms.Button btnS4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblS5;
+        private System.Windows.Forms.Button btnS5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SpellManager/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Timers;
+
+namespace SpellManager
+{
+    public partial class Form1 : Form
+    {
+        //マウスのクリック位置を記憶
+        private Point mousePoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Opacity = 0.8;
+            this.MinimumSize = new Size(0, 0);
+            this.Size = new Size(100, 250);
+
+            int radius = 4;
+            int diameter = radius * 2;
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+
+            // 左上
+            gp.AddPie(0, 0, diameter, diameter, 180, 90);
+            // 右上
+            gp.AddPie(this.Width - diameter, 0, diameter, diameter, 270, 90);
+            // 左下
+            gp.AddPie(0, this.Height - diameter, diameter, diameter, 90, 90);
+            // 右下
+            gp.AddPie(this.Width - diameter, this.Height - diameter, diameter, diameter, 0, 90);
+            // 中央
+            gp.AddRectangle(new Rectangle(radius, 0, this.Width - diameter, this.Height));
+            // 左
+            gp.AddRectangle(new Rectangle(0, radius, radius, this.Height - diameter));
+            // 右
+            gp.AddRectangle(new Rectangle(this.Width - radius, radius, radius, this.Height - diameter));
+
+            this.Region = new Region(gp);
+
+            SetLabelSetting(btnS1, lblS1);
+            SetLabelSetting(btnS2, lblS2);
+            SetLabelSetting(btnS3, lblS3);
+            SetLabelSetting(btnS4, lblS4);
+            SetLabelSetting(btnS5, lblS5);
+        }
+
+        private void SetLabelSetting(Button btn, Label lbl)
+        {
+            btn.Controls.Add(lbl);
+            lbl.BackColor = Color.FromArgb(200, Color.Gray);
+            lbl.Top = lbl.Top - btn.Top;
+            lbl.Left = lbl.Left - btn.Left;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                //位置を記憶する
+                mousePoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                this.Left += e.X - mousePoint.X;
+                this.Top += e.Y - mousePoint.Y;
+            }
+        }
+        private void SetTimer(Label lbl)
+        {
+            lbl.Visible = true;
+
+            // タイマーの間隔(ミリ秒)
+            var timer = new System.Timers.Timer(1000);
+            //var time = new DateTime(2000, 1, 1, 0, 0, 3, 0);
+            var time = new DateTime(2000, 1, 1, 0, 5, 0, 0);
+            lbl.Text = time.ToString("m:ss");
+
+            // タイマーの処理
+            timer.Elapsed += (s, ee) =>
+            {
+                time = time.AddSeconds(-1);
+                lbl.Text = time.ToString("m:ss");
+                if (time.Minute == 0 && time.Second == 0)
+                {
+                    timer.Stop();
+                    lbl.Visible = false;
+                }
+            };
+
+            // タイマーを開始する
+            timer.Start();
+        }
+
+        private void btnS1_Click(object sender, EventArgs e)
+        {
+            SetTimer(lblS1);
+        }
+
+        private void btnS2_Click(object sender, EventArgs e)
+        {
+            SetTimer(lblS2);
+        }
+
+        private void btnS3_Click(object sender, EventArgs e)
+        {
+            SetTimer(lblS3);
+        }
+
+        private void btnS4_Click(object sender, EventArgs e)
+        {
+            SetTimer(lblS4);
+        }
+
+        private void btnS5_Click(object sender, EventArgs e)
+        {
+            SetTimer(lblS5);
+        }
+    }
+}
